45b6ca3e5c1a0a20dbadaa6554ec21ec85e0c530
jkb4y <jkb4y@york.cphg.Virginia.EDU>
2012-10-26 12:02:08 -0400

    manhattan_ticks uses actual SNP ranges to determine locations of tick marks on x-axis

---
+++ manhattan_ticks.R
@@ -0,0 +1,315 @@
+
+# Original manhattan code forked from:
+# Stephen Turner
+# http://StephenTurner.us/
+# http://GettingGeneticsDone.blogspot.com/
+# See license at http://gettinggeneticsdone.blogspot.com/p/copyright.html
+
+# Last updated: Tuesday, April 19, 2011
+# R code for making manhattan plots and QQ plots from plink output files.
+# manhattan() with GWAS data this can take a lot of memory, recommended for use on 64bit machines only, for now.
+# Altnernatively, use bmanhattan() , i.e., base manhattan. uses base graphics. way faster.
+
+
+## This is for testing purposes.
+# set.seed(42)
+# nchr=23
+# nsnps=1000
+# d=data.frame(
+# SNP=sapply(1:(nchr*nsnps), function(x) paste("rs",x,sep='')),
+# CHR=rep(1:nchr,each=nsnps),
+# BP=rep(1:nsnps,nchr),
+# P=runif(nchr*nsnps)
+# )
+# annotatesnps <- d$SNP[7550:7750]
+
+
+#plot all of Achilleas cis-eQTL data files
+achilleas_all <- function(perm=FALSE, exon=FALSE, flag='', file_type='jpeg', ymin=-log(1e-4)){
+	for (cell in c("B","CD4","CD8","MONO","NK")){
+		achilleas_cis(cell_type=cell, perm=perm,exon=exon,flag=flag,ymin=ymin)
+	}
+
+}
+
+# plot Achilleas' cis-EQTL data, taking a perm flag if it is a perm file
+achilleas_cis <-function(cell_type = NULL, perm = FALSE, exon = FALSE, flag = NULL, file_type = 'jpeg',colors=c("navy","darkorange"), genomewideline=F,ymin=0,...){
+data_folder = "/home/jkb4y/ubs/work/data/Achilleas/cis-eQTLs_Oct2012"
+permflag = ''
+transflag = 'transcript'
+transtitle = 'Transcript'
+if (perm==TRUE){
+permflag = '_perm'}
+data_name = paste0(cell_type,'_cis_transcript_eqtls',permflag,'_all_new_noNA_JB.txt')
+if (exon==TRUE){
+	transflag = 'exon'
+	data_name = paste0(cell_type,'_exon_cis_eqtl',permflag,'_results_noNA_JB.txt')
+	transtitle = 'Exon'
+	}
+#data_name = paste0(cell_type,'_cis_transcript_eqtls',permflag,'_all_new_JB.txt')
+data_loc = file.path(data_folder, data_name)
+print(data_loc)
+#if (perm==TRUE){
+#permflag = '_perm'
+#}
+if (!(flag=='')){
+permflag = paste0(permflag,"_",flag)}
+out_folder =file.path("/home/jkb4y/ubs/work/results/Achilleas/cis-eQTLs_Oct2012",transflag,paste0(cell_type,permflag),"Manhattans")
+if (! file.exists(out_folder)){
+		dir.create(out_folder)
+		}
+pdf_out_name = paste0(cell_type, "_cis_eqtls_",transflag,permflag,".pdf")
+pdf_out_loc = file.path(out_folder, pdf_out_name)
+png_out_loc = file.path(out_folder, paste0(cell_type, "_cis_eqtls_",transflag,permflag,".png"))
+ps_out_loc = file.path(out_folder, paste0(cell_type, "_cis_eqtls_",transflag,permflag,".ps"))
+jpg_out_loc = file.path(out_folder, paste0(cell_type, "_cis_eqtls_",transflag,permflag,".jpeg"))
+if (file_type == 'jpeg'){out_loc = jpg_out_loc}
+else if (file_type == 'ps'){out_loc = ps_out_loc}
+#out_loc = file.path(out_folder, out_name)
+print(out_loc)
+
+title = paste(cell_type,"Cells: Local ",transtitle," eQTLs")
+if (perm){title = paste(title, "(perm)")}
+
+
+
+
+results <- read.table(data_loc, T)
+#print(tail(results))
+#postscript(file=ps_out_loc, paper="letter", horizontal=T, title=title)
+#pdf(out_loc, title=title, width=11, height=8.5) #,paper="letter", horizontal=T)
+#png(png_out_loc, quality=100)
+jpeg(jpg_out_loc, width=1100, height=850, quality=100)
+manhattan(results, colors=colors, main=title, suggestiveline=F, genomewideline=genomewideline, pch=16, limitchromosomes=1:23,ymin=ymin)
+dev.off()
+
+}
+
+
+# plot Achilleas' trans-EQTL data
+achilleas_trans <-function(cell_type = NULL, flag = NULL, file_type = 'jpeg',colors=c("navy","darkorange"), genomewideline=F,ymin=0,...){
+data_folder = "/home/jkb4y/ubs/work/data/Achilleas/transcript_eqtl_results_Oct12"
+
+data_name = paste0(cell_type,'_all_data_JB.txt')
+
+data_loc = file.path(data_folder, data_name)
+print(data_loc)
+
+out_folder =file.path("/home/jkb4y/ubs/work/results/Achilleas/transcript_eqtl_results_Oct12",cell_type,"Manhattans")
+if (! file.exists(out_folder)){
+		dir.create(out_folder)
+		}
+if (is.null(flag)) { addflag=''}
+else{ addflag = paste0('_',flag)}
+pdf_out_name = paste0(cell_type, "_trans_eqtls",addflag,".pdf")
+pdf_out_loc = file.path(out_folder, pdf_out_name)
+png_out_loc = file.path(out_folder, paste0(cell_type, "_trans_eqtls",addflag,".png"))
+ps_out_loc = file.path(out_folder, paste0(cell_type, "_trans_eqtls",addflag,".ps"))
+jpg_out_loc = file.path(out_folder, paste0(cell_type, "_trans_eqtls",addflag,".jpeg"))
+
+
+
+title = paste(cell_type,"Cells: Trans eQTLs")
+
+results <- read.table(data_loc, T)
+results <- results[results$TRANS.CIS == "trans", ]
+print(head(results))
+if (file_type == 'ps'){
+#print(tail(results))
+postscript(file=ps_out_loc, paper="letter", horizontal=T, title=title)
+}
+else if (file_type == 'pdf'){
+pdf(pdf_out_loc, title=title, width=11, height=8.5) #,paper="letter", horizontal=T)
+}
+else if (file_type == 'png') {
+png(png_out_loc, quality=100)
+}
+else{
+jpeg(jpg_out_loc, width=1100, height=850, quality=100)
+}
+manhattan(results, colors=colors, main=title, suggestiveline=F, genomewideline=genomewideline, pch=16, limitchromosomes=1:23,ymin=ymin)
+dev.off()
+
+}
+
+
+# manhattan plot using base graphics
+manhattan <- function(dataframe, colors=c("navy","darkorange"), ymax="max", limitchromosomes=1:23,
+ suggestiveline=F, genomewideline=F, annotate=NULL, ymin=0,...) {
+#suggestiveline=-log10(1e-5), genomewideline=-log10(5e-8), annotate=NULL, ...) {
+    d=dataframe
+    if (!("CHR" %in% names(d) & "BP" %in% names(d) )) stop("Make sure your data frame contains columns CHR and BP")
+    if (!("P" %in% names(d) | "EMP1" %in% names(d))) stop("Make sure your data frame contains EITHER column P or column EMP1")
+    if("EMP1" %in% names(d)) d$P = d$EMP1;
+    
+    if (any(limitchromosomes)) d=d[d$CHR %in% limitchromosomes, ]
+    d=subset(na.omit(d[order(d$CHR, d$BP), ]), (P>0 & P<=1)) # remove na's, sort, and keep only 0<P<=1
+    d$logp = -log10(d$P)
+    d$pos=NA
+    ticks=NULL
+    lastbase=0
+    colors <- rep(colors,max(d$CHR))[1:max(d$CHR)]
+    if (ymax=="max") ymax<-ceiling(max(d$logp))
+    if (ymax<8) ymax<-8
+    
+    numchroms=length(unique(d$CHR))
+    if (numchroms==1) {
+        d$pos=d$BP
+        ticks=floor(length(d$pos))/2+1
+    } else {
+        for (i in unique(d$CHR)) {
+          if (i==1) {
+     d[d$CHR==i, ]$pos=d[d$CHR==i, ]$BP
+     } else {
+     
+     lastbase=lastbase+tail(subset(d,CHR==i-1)$BP, 1)
+     print(lastbase)
+     d[d$CHR==i, ]$pos=d[d$CHR==i, ]$BP+lastbase
+     }
+     ordered = order(d[d$CHR==i,]$BP)
+     len = length(ordered)
+	 first <- as.numeric(d[d$CHR==i,]$pos[ ordered[1] ]);
+     last <- as.numeric(d[d$CHR==i,]$pos[ ordered[len]] );
+     mid = (last - first)/2
+     middle = first + mid
+     print(c(first, last, middle))
+     ordered = NULL
+     ticks=c(ticks, middle)
+     #ticks=c(ticks, d[d$CHR==i, ]$pos[floor(length(d[d$CHR==i, ]$pos)/2)+1])
+     print(ticks)
+     }
+    }
+    
+    if (numchroms==1) {
+        with(d, plot(pos, logp, ylim=c(ymin,ymax), ylab=expression(-log[10](italic(p))), xlab=paste("Chromosome",unique(d$CHR),"position"), ...))
+    }	else {
+        with(d, plot(pos, logp, ylim=c(ymin,ymax), ylab=expression(-log[10](italic(p))), xlab="Chromosome", xaxt="n", type="n", ...))
+        axis(1, at=ticks, lab=unique(d$CHR), ...)
+        icol=1
+        for (i in unique(d$CHR)) {
+            with(d[d$CHR==i, ],points(pos, logp, col=colors[icol], ...))
+            icol=icol+1
+     }
+    }
+    
+    if (!is.null(annotate)) {
+        d.annotate=d[which(d$SNP %in% annotate), ]
+        with(d.annotate, points(pos, logp, col="green3", ...))
+    }
+    
+    if (suggestiveline) abline(h=suggestiveline, col="blue")
+    if (genomewideline) abline(h=genomewideline, col="red")
+}
+
+
+## Make a pretty QQ plot of p-values
+qq = function(pvector, ...) {
+if (!is.numeric(pvector)) stop("D'oh! P value vector is not numeric.")
+pvector <- pvector[!is.na(pvector) & pvector<1 & pvector>0]
+o = -log10(sort(pvector,decreasing=F))
+e = -log10( ppoints(length(pvector) ))
+plot(e,o,pch=19,cex=1, xlab=expression(Expected~~-log[10](italic(p))), ylab=expression(Observed~~-log[10](italic(p))), xlim=c(0,max(e)), ylim=c(0,max(o)), ...)
+abline(0,1,col="red")
+}
+
+
+### OLD GGPLOT2 CODE ###
+
+# manhattan plot using ggplot2
+gg.manhattan = function(dataframe, title=NULL, max.y="max", suggestiveline=0, genomewideline=-log10(5e-8), size.x.labels=9, size.y.labels=10, annotate=F, SNPlist=NULL) {
+library(ggplot2)
+    if (annotate & is.null(SNPlist)) stop("You requested annotation but provided no SNPlist!")
+d=dataframe
+#limit to only chrs 1-23?
+d=d[d$CHR %in% 1:23, ]
+if ("CHR" %in% names(d) & "BP" %in% names(d) & "P" %in% names(d) ) {
+d=na.omit(d)
+d=d[d$P>0 & d$P<=1, ]
+d$logp = -log10(d$P)
+d$pos=NA
+ticks=NULL
+lastbase=0
+#new 2010-05-10
+numchroms=length(unique(d$CHR))
+if (numchroms==1) {
+d$pos=d$BP
+} else {
+
+for (i in unique(d$CHR)) {
+if (i==1) {
+d[d$CHR==i, ]$pos=d[d$CHR==i, ]$BP
+}	else {
+lastbase=lastbase+tail(subset(d,CHR==i-1)$BP, 1)
+d[d$CHR==i, ]$pos=d[d$CHR==i, ]$BP+lastbase
+}
+ticks=c(ticks, d[d$CHR==i, ]$pos[floor(length(d[d$CHR==i, ]$pos)/2)+1])
+}
+ticklim=c(min(d$pos),max(d$pos))
+
+}
+mycols=rep(c("gray10","gray60"),max(d$CHR))
+if (max.y=="max") maxy=ceiling(max(d$logp)) else maxy=max.y
+if (maxy<8) maxy=8
+if (annotate) d.annotate=d[as.numeric(substr(d$SNP,3,100)) %in% SNPlist, ]
+if (numchroms==1) {
+plot=qplot(pos,logp,data=d,ylab=expression(-log[10](italic(p))), xlab=paste("Chromosome",unique(d$CHR),"position"))
+}	else {
+plot=qplot(pos,logp,data=d, ylab=expression(-log[10](italic(p))) , colour=factor(CHR))
+plot=plot+scale_x_continuous(name="Chromosome", breaks=ticks, labels=(unique(d$CHR)))
+plot=plot+scale_y_continuous(limits=c(0,maxy), breaks=1:maxy, labels=1:maxy)
+plot=plot+scale_colour_manual(value=mycols)
+}
+if (annotate) plot=plot + geom_point(data=d.annotate, colour=I("green3"))
+plot=plot + opts(legend.position = "none")
+plot=plot + opts(title=title)
+plot=plot+opts(
+panel.background=theme_blank(),
+panel.grid.minor=theme_blank(),
+axis.text.x=theme_text(size=size.x.labels, colour="grey50"),
+axis.text.y=theme_text(size=size.y.labels, colour="grey50"),
+axis.ticks=theme_segment(colour=NA)
+)
+if (suggestiveline) plot=plot+geom_hline(yintercept=suggestiveline,colour="blue", alpha=I(1/3))
+if (genomewideline) plot=plot+geom_hline(yintercept=genomewideline,colour="red")
+plot
+}	else {
+stop("Make sure your data frame contains columns CHR, BP, and P")
+}
+}
+
+gg.qq = function(pvector, title=NULL, spartan=F) {
+library(ggplot2)
+o = -log10(sort(pvector,decreasing=F))
+#e = -log10( 1:length(o)/length(o) )
+e = -log10( ppoints(length(pvector) ))
+plot=qplot(e,o, xlim=c(0,max(e)), ylim=c(0,max(o))) + stat_abline(intercept=0,slope=1, col="red")
+plot=plot+opts(title=title)
+plot=plot+scale_x_continuous(name=expression(Expected~~-log[10](italic(p))))
+plot=plot+scale_y_continuous(name=expression(Observed~~-log[10](italic(p))))
+if (spartan) plot=plot+opts(panel.background=theme_rect(col="grey50"), panel.grid.minor=theme_blank())
+plot
+}
+
+gg.qqman = function(data="plinkresults") {
+myqqplot = ggqq(data$P)
+mymanplot = ggmanhattan(data)
+ggsave(file="qqplot.png",myqqplot,w=5,h=5,dpi=100)
+ggsave(file="manhattan.png",mymanplot,width=12,height=9,dpi=100)
+}
+
+gg.qqmanall= function(command="ls *assoc") {
+filelist=system(command,intern=T)
+datalist=NULL
+for (i in filelist) {datalist[[i]]=read.table(i,T)}
+highestneglogp=ceiling(max(sapply(datalist, function(df) max(na.omit(-log10(df$P))))))
+print(paste("Highest -log10(P) = ",highestneglogp),quote=F)
+start=Sys.time()
+for (i in names(datalist)) {
+myqqplot=ggqq(datalist[[i]]$P, title=i)
+ggsave(file=paste("qqplot-", i, ".png", sep=""),myqqplot, width=5, height=5,dpi=100)
+mymanplot=ggmanhattan(datalist[[i]], title=i, max.y=highestneglogp)
+ggsave(file=paste("manhattan-", i, ".png", sep=""),mymanplot,width=12,height=9,dpi=100)
+}
+end=Sys.time()
+print(elapsed<-end-start)
+}
